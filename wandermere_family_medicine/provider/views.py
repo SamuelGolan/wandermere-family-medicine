15a6e9b1e6298431bbf556d59670309204e25cb5
SamuelGolan <93092697+SamuelGolan@users.noreply.github.com>
2022-11-19 02:13:29 -0800

    adding bootstrap

--- wandermere_family_medicine/provider/views.py
+++ wandermere_family_medicine/provider/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .import models
 # Create your views here.
 
 def provider_view(request, firstname, lastname):
-    return render('provider/provider.html')
+    provider = get_object_or_404(models.Provider, firstname=firstname, lastname=lastname)
+    return render(request, 'provider/provider.html', vars(provider))
