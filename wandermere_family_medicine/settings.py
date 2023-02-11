d05508b2a41fa347398f5817b24c0f6314ce767e
SamuelGolan <93092697+SamuelGolan@users.noreply.github.com>
2023-02-10 22:41:30 -0800

    Update settings.py

--- wandermere_family_medicine/settings.py
+++ wandermere_family_medicine/settings.py
@@ -72,10 +72,10 @@ STATICFILES_FINDERS = [
 STATIC_URL = "/static/"
 
 # if not DEBUG:
-# # if True:
-#     # Tell Django to copy statics to the `staticfiles` directory
-#     # in your application directory on Render.
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if True:
+    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #     # Turn on WhiteNoise storage backend that takes care of compressing static files
 #     # and creating unique names for each version so they can safely be cached forever.
