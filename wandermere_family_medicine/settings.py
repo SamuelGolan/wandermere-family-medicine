84f82018457c9a6d3a28e1c75e869e8b465d2f27
SamuelGolan <93092697+SamuelGolan@users.noreply.github.com>
2023-02-10 22:13:17 -0800

    Update settings.py

--- wandermere_family_medicine/settings.py
+++ wandermere_family_medicine/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', default=None)
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'RENDER' not in os.environ
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -71,8 +71,8 @@ STATICFILES_FINDERS = [
 
 STATIC_URL = "/static/"
 
-# if not DEBUG:
-if True:
+if not DEBUG:
+# if True:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
