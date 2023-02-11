0b5ab85301e068a2e01ef4692baf3d1544c5936b
SamuelGolan <93092697+SamuelGolan@users.noreply.github.com>
2023-02-11 01:43:06 -0800

    default provider image

--- provider/models.py
+++ provider/models.py
@@ -16,7 +16,7 @@ class Provider(models.Model):
     email = models.EmailField(blank=True)
     bio = models.TextField(blank=True)
     name_slug = models.SlugField(max_length=50, editable=False, blank=True)
-    image = models.ImageField(blank=True, upload_to='profile_pics')
+    image = models.ImageField(upload_to='profile_pics', default='static/img/default_provider.jpg')
 
     def __str__(self):
         return f"{self.firstname} {self.lastname} {self.degree}"
@@ -27,13 +27,10 @@ class Provider(models.Model):
             self.name_slug = slugify(f'{self.firstname} {self.lastname}', lowercase=False)
             img = Image.open(self.image.path)
             if img.width != img.height:
-                print(img.height, img.width)
                 if img.height > img.width:
-                    print(False)
                     diff = img.height - img.width
                     img = img.crop((0, int(diff//2), img.width, int(img.height-diff//2)))
                 elif img.width > img.height:
-                    print(True)
                     diff = img.width - img.height
                     img = img.crop((int(diff//2), 0, int(img.width-diff//2), img.height))
                 img.save(self.image.path)
