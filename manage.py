3f142c22bbec967e3e082e17cc748a0e60e2dd90
SamuelGolan <93092697+SamuelGolan@users.noreply.github.com>
2023-02-10 18:08:38 -0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wandermere-family-medcine.wandermere_family_medicine.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', '/wandermere_family_medicine.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
