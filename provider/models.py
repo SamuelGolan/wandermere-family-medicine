897c8da66fde5561d5ce374a09f0688fd1ae678e
SamuelGolan <93092697+SamuelGolan@users.noreply.github.com>
2023-02-12 19:47:53 -0800

    Update models.py

--- provider/models.py
+++ provider/models.py
@@ -23,22 +23,22 @@ class Provider(models.Model):
         return f"{self.firstname} {self.lastname} {self.degree}"
     
     # TODO implement provider image
-    # def save(self, *args, **kwargs):
-    #     print("Saving Image")
-    #     try:
-    #         self.name_slug = slugify(f'{self.firstname} {self.lastname}', lowercase=False)
-    #         img = Image.open(self.image.path)
-    #         if img.width != img.height:
-    #             if img.height > img.width:
-    #                 diff = img.height - img.width
-    #                 img = img.crop((0, int(diff//2), img.width, int(img.height-diff//2)))
-    #             elif img.width > img.height:
-    #                 diff = img.width - img.height
-    #                 img = img.crop((int(diff//2), 0, int(img.width-diff//2), img.height))
-    #             img.save(self.image.path)
-    #         if img.height > 300 or img.width > 300:
-    #             img.thumbnail((300, 300))
-    #             img.save(self.image.path)
-    #         super().save(*args, **kwargs)
-    #     except Exception as err:
-    #         print(err)
+    def save(self, *args, **kwargs):
+        print("Saving Image")
+        try:
+            self.name_slug = slugify(f'{self.firstname} {self.lastname}', lowercase=False)
+            # img = Image.open(self.image.path)
+            # if img.width != img.height:
+            #     if img.height > img.width:
+            #         diff = img.height - img.width
+            #         img = img.crop((0, int(diff//2), img.width, int(img.height-diff//2)))
+            #     elif img.width > img.height:
+            #         diff = img.width - img.height
+            #         img = img.crop((int(diff//2), 0, int(img.width-diff//2), img.height))
+            #     img.save(self.image.path)
+            # if img.height > 300 or img.width > 300:
+            #     img.thumbnail((300, 300))
+            #     img.save(self.image.path)
+            # super().save(*args, **kwargs)
+        except Exception as err:
+            print(err)
