3a28e27fe0f611b28962c692d587b697ff5082be
SamuelGolan <93092697+SamuelGolan@users.noreply.github.com>
2023-02-12 18:59:41 -0800

    Update settings.py

--- wandermere_family_medicine/settings.py
+++ wandermere_family_medicine/settings.py
@@ -126,7 +126,7 @@ else:
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'wfm.serviceuser@gmail.com'
+EMAIL_HOST_USER = os.environ.get('SERVICE_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('SERVICE_USER_PASS')
 EMAIL_PORT = 587
 # Password validation
